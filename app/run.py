53d190c58e1d4ba4eda5c61295fa030c02e1947b
Leyla A <alieva91@gmail.com>
2024-10-27 14:42:37 +0100

    Update filepaths and add two new visualizations

--- app/run.py
+++ app/run.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-
 from flask import Flask
 from flask import render_template, request, jsonify
 from plotly.graph_objs import Bar
